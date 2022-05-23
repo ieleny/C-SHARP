ea93123c9ae230e16595c9252b6ca35833bf7987
Ieleny Filgueira <ieleny.filgueira@take.net>
2022-05-23 10:07:58 -0300

    Organizando

---
+++ Estudo antigo/Variaveis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Variaveis
+    {
+        static void main(string[] args)
+        {
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+        }
+                        
+    }
+}
