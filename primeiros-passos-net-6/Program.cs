cdc0e78e4b32d11a43afc2c80daff56bc5896307
Ieleny <ielenyfilgueira@hotmail.com>
2022-05-24 17:34:51 -0300

    Criado a classe pilha e nova pasta

---
+++ primeiros-passos-net-6/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Versão > 6.1 : Console.WriteLine("Hello, World!");
+// https://docs.microsoft.com/pt-pt/dotnet/core/tutorials/top-level-templates
+
+using System;
+using PrimeirosPassosNet.estruturaDados;
+
+namespace PrimeirosPassosNet // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Primeira Aula
+            int quantidadeVezes = 100;
+
+            for(int contador = 1; contador <= quantidadeVezes; contador++){
+                Console.WriteLine(" Contador: " + contador + " Quantidade de vezes: " +  quantidadeVezes);
+                Console.WriteLine($" Contador: {contador} Quantidade de vezes: {quantidadeVezes}");
+            }
+
+            Console.WriteLine("/");
+
+            // Segunda aula
+            var pilha = new Pilha();
+
+            pilha.Empilha(2);
+            pilha.Empilha(3);
+            pilha.Empilha(1);
+
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+
+            // Testar o exception
+            // Console.WriteLine(pilha.Desempilha());
+
+        }
+    }
+}
---
+++ primeiros-passos-net-6/README.md
@@ -0,0 +1,18 @@
+# Comandos
+
+## Restore 
+> Dotnet restore
+
+Restaurar os pacotes do arquivo primeiros-passos-net-6.csproj
+
+## Build
+
+> Dotnet build
+
+Restaurar o pacotes, e pegar o projeto da fonte e gerar os dlls, executaveis, e binarios.
+
+## Run
+
+> Dotnet run
+
+Exeutar o projeto, (Apenas no arquivo csproj)
